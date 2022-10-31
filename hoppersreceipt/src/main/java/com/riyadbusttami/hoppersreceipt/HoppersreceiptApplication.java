9c5b9ca423e463674e3e0e232019da37b8a6c6d2
Riyad Busttami <Riyad.Bustami@gmail.com>
2022-10-31 20:45:24 +0200

    done

---
+++ hoppersreceipt/src/main/java/com/riyadbusttami/hoppersreceipt/HoppersreceiptApplication.java
@@ -0,0 +1,16 @@
+package com.riyadbusttami.hoppersreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoppersreceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppersreceiptApplication.class, args);
+	}
+    
+    
+
+
+}
